1c16ad888e86807cba118352f5107ad722c2d4ca
safaaltifi <safaa.ltifi123@gmail.com>
2025-04-12 09:20:56 +0100

    first commit

---
+++ src/main/java/org/example/safaa_ltifi_spring/SafaaLtifiSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.safaa_ltifi_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafaaLtifiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafaaLtifiSpringApplication.class, args);
+    }
+
+}
